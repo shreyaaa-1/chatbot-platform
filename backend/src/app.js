32286d674241e1213992d2b4582e72deb06dea49
Shreya Singh <shreyasingh47604@gmail.com>
2026-01-18 01:55:51 +0530

    Prepare backend for cloud deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,8 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
